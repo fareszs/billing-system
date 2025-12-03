03ca76388698ce0e15246fe9fdca57edcf7aa542
omarmohamed028 <omohamedsalah72@gmail.com>
2025-12-03 22:22:14 +0200

    Update Tariff.java

--- Code/Project/src/main/java/com/mycompany/project/Tariff.java
+++ Code/Project/src/main/java/com/mycompany/project/Tariff.java
@@ -1,1 +1,31 @@
+public class Tariff {
+
+    private static String region;
+    private static double pricePerUnit;
+
+    public static void init(String reg, double price) {
+        region = reg;
+        pricePerUnit = price;
+    }
+
+    public static String getRegion() {
+        return region;
+    }
+
+    public static void setRegion(String reg) {
+        region = reg;
+    }
+
+    public static double getPricePerUnit() {
+        return pricePerUnit;
+    }
+
+    public static void setPricePerUnit(double price) {
+        pricePerUnit = price;
+    }
+
+    public static String toStr() {
+        return "Tariff [region=" + region + ", pricePerUnit=" + pricePerUnit + "]";
+    }
+}
 

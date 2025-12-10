9af335588fba3e642d7a0b5cdc72096e1c9c7304
omarmohamed028 <omohamedsalah72@gmail.com>
2025-12-10 12:47:25 +0200

    Update Admin.java

--- Code/Project/src/main/java/com/mycompany/project/Admin.java
+++ Code/Project/src/main/java/com/mycompany/project/Admin.java
@@ -1,1 +1,13 @@
+package com.mycompany.project;
+
+public class Admin extends Operator {
+
+    public Admin() {
+    }
+
+    public static void viewBillsForRegion(String region) {
+        // Call the Operator method and return same output
+        Operator.viewBillsByRegion(region);
+    }
+}
 
